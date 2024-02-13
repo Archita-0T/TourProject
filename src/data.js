0294c1321be088c4872c1fe2d99a67dbc6231517
unknown <architatiwari961@gmail.com>
2024-02-14 02:09:46 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const data=[
+    {
+        id:1,
+        cityName:"Agra",
+        info:" is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres (140 mi) south-east of the national capital Delhi and 330 km west of the state capital Lucknow. With a population of roughly 1.6 million, Agra is the fourth-most populous city in Uttar Pradesh and twenty-third most populous city in India.",
+        image:"https://cdn.thecodehelp.in/Agra.jpeg",
+        price:"35,758",
+    },
+    {
+        id:2,
+        cityName:"Jaipur",
+        info:"formerly known as Jeypore, is the capital and the largest city of the Indian state of Rajasthan. As of 2011, the city has a population of 3.1 million, making it the tenth most populous city in the country. Located 268 km (167 miles) from the national capital New Delhi, Jaipur is also known as the Pink City due to the dominant color scheme of its buildings in old city",
+        image:"https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price:"82,560",
+    },
+    {
+        id:3,
+        cityName:"Goa",
+        info:"The ancient Hindu city of Goa, hardly a fragment of which survives, was built at the southernmost point of the island of Goa. The city was famous in early Hindu legend and history; in the Puranas and various inscriptions, its name appears as Gove, Govapuri, and Gomant. The medieval Arabian geographers knew it as Sindabur, or Sandābūr, and the Portuguese called it Velha Goa. It was ruled by the Kadamba dynasty from the 2nd century CE to 1312 and by Muslim invaders of the Deccan from 1312 to 1367. The city was then annexed by the Hindu kingdom of Vijayanagar and was later conquered by the Bahmanī sultanate, which founded Old Goa on the island in 1440.",
+        image:"https://cdn.thecodehelp.in/Goa.jpeg",
+        price:"29,695",
+    },
+    {
+        id:4,
+        cityName:"Varanasi",
+        info:"Varanasi is one of the oldest continuously inhabited cities in the world. It was one of the first major urban settlements in the middle Ganges valley. By the 2nd millennium BCE Varanasi was a seat of Vedic religion and philosophy and was also a commercial and industrial centre famous for its muslin and silk fabrics, perfumes, ivory works, and sculpture. It was the capital of the kingdom of Kashi during the time of the Buddha (6th century BCE), who gave his first sermon nearby at Sarnath. Varanasi remained a centre of religious, educational, and artistic activities as attested by the celebrated Chinese Buddhist pilgrim Xuanzang, who visited it about 635 CE and said that the city extended for about 3 miles (5 km) along the western bank of the Ganges.",
+        image:"https://cdn.thecodehelp.in/Varanasi.jpeg",
+        price:"31,095",
+    },
+    {
+        id:5,
+        cityName:"Darjeeling",
+        info:"Darjeeling, city, extreme northern West Bengal state, northeastern India. It lies about 305 miles (490 km) north of Kolkata, at an elevation of about 7,000 feet (2,100 metres) above sea level. Darjeeling, India is situated on a long, narrow mountain ridge of the Sikkim Himalayas that descends abruptly to the bed of the Great Rangit River. On a clear day the city affords a magnificent view of Kanchenjunga (28,169 feet [8,586 metres]), and Mount Everest can be seen from a nearby viewing point. The name of the city means “place of the thunderbolt.” Darjeeling is a hill resort, and the city’s economy is based largely on tourism; the peak periods for visitors are April to June and September to November. The city has major road, rail, and air connections with Kolkata.",
+        image:"https://cdn.thecodehelp.in/Darjeeling.jpeg",
+        price:"78,595",
+    },
+    {
+        id:6,
+        cityName:"Jaisalmer",
+        info:"Jaisalmer, town, western Rajasthan state, northwestern India. It is situated on a level plain in the Thar (Great Indian) Desert, about 135 miles (215 km) northwest of Jodhpur. The town, noted for its buildings of yellowish brown stone, was founded in 1156 by Rawal Jaisal, a chief of the Rajputs (the warrior rulers of the historic region of Rajputana). In the 12th century Jaisalmer state reached the height of its power. After the sack of the capital by the Muslim Delhi sultan ʿAlāʾ al-Dīn Khaljī early in the 14th century, its fortunes declined. It subsequently became a Mughal fiefdom and entered into political relations with the British in 1818. In 1949 it joined the state of Rajasthan.",
+        image:"https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+        price:"68,758",
+    },
+    {
+        id:7,
+        cityName:"Kochi",
+        info:"Kochi, city and major port on the Malabar Coast of the Arabian Sea, west-central Kerala state, southwestern India. Also the name of a former princely state, “Kochi” is sometimes used to refer to a cluster of islands and towns, including Ernakulam, Mattancheri, Fort Cochin, Willingdon Island, Vypin Island, and Gundu Island. The urban agglomeration includes the localities of Trikkakara, Eloor, Kalamassery, and Trippunithura. Kochi was an insignificant fishing village until, in the 14th century, the backwaters of the Arabian Sea and the streams descending from the Western Ghats to the east caused the separation of the village from the mainland, turning the landlocked harbour into one of the safest ports on India’s southwestern coast. The port assumed a new strategic importance and began to experience commercial prosperity.",
+        image:"https://cdn.thecodehelp.in/Kochi.jpeg",
+        price:"45,758",
+    }
+];
+export default data;
